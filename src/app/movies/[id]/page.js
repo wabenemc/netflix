75cbab863e8143b92e6306e055b2adac38f145a0
Wabene cedric <cedric.wabenemayele.simplon@gmail.com>
2024-05-24 16:48:30 +0200

    detail_movies

--- src/app/movies/[id]/page.js
+++ src/app/movies/[id]/page.js
@@ -1,30 +1,28 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
 
-export default function MovieDetails(props) {
+import React, { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+
+export default function MovieDetails() {
   const [movie, setMovie] = useState({});
   const { id } = useParams();
-
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(
+      const response = await fetch(
         `https://api.themoviedb.org/3/movie/${id}?api_key=3f2761ba67df964d1ec535335fa4ef9a`
       );
-      setMovie(response.data);
+      const data = await response.json();
+      setMovie(data);
     };
+
     fetchData();
   }, [id]);
 
+  const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
   return (
     <div className="movie-details">
-      {movie.title && <p>{movie.title}</p>}
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
-      />
       <h2>{movie.title}</h2>
+      <img src={imageUrl} alt={movie.title} />
       <p>{movie.overview}</p>
     </div>
   );
